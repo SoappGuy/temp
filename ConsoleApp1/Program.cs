b34785527f1378473c19e7ad094ba3b06c593f12
dumbnerd <107756166+SoappGuy@users.noreply.github.com>
2024-03-04 21:38:40 +0200

    refactored sum

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+static int Sum(int a, int b)
+{
+    return a + b;
+}
 
-int a = 2;
-int b = 3;
-int sum = a + b;
+int sum = Sum(2, 3);
 Console.WriteLine("sum: " + sum);
 Console.WriteLine("Hello, git!");
